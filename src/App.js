c957ade12cffaab8ef8b3d6cca241bd3ce3e35fd
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-11-01 20:28:32 +0200

    fetch->state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
+import GAPI from './GoogleApi/GoogleApi';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Google Book Search</p>
-      </header>
-    </div>
+        <GAPI >
+          <p>Yo!</p>
+          <p>Yo!</p>
+          <p>Yo!</p>
+          <p>Yo!</p>
+          <p>Yo!</p>
+          <p>Yo!</p>
+        </GAPI>
+      </header >
+    </div >
   );
 }
 
--- src/GoogleApi/GoogleApi.js
+++ src/GoogleApi/GoogleApi.js
@@ -1,49 +1,63 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './GoogleApi.css'
+import { throwStatement } from '@babel/types';
 
-class GoogleApi extends Component{
-    Constructor(props) {
-        super(props)
-        this.state = {
-            search: "",
-            PrintType: [],
-            BookType: []
+class GoogleApi extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      search: "",
+      PrintType: [],
+      BookType: [],
+      items: [],
 
-        }
     }
-    componentDidMount() {
-        const url = 'https://www.googleapis.com/books/v1/volumes?q=search+terms'
-        const options = {
-            method: 'GET',
-            headers: {
-                "APIKey": 'AIzaSyDjWWRGP6jDjn_ZVzS4W8ISZ0h30afpkaw',
-                'Content-Type': "application/json"
-            }
+  }
+  render() {
+    return (
+      <>
+        {this.state.items.map(i => { return (<li>{i.volumeInfo.title}</li>) })}
+      </>
+    )
+  }
+  componentDidMount() {
+    const url = 'https://www.googleapis.com/books/v1/volumes?q=search+terms'
+    const options = {
+      method: 'GET',
+      headers: {
+        "APIKey": 'AIzaSyDjWWRGP6jDjn_ZVzS4W8ISZ0h30afpkaw',
+        'Content-Type': "application/json"
+      },
+      items: [],
 
-        }
+    }
 
     fetch(url, options)
-    .then(res => {
-      if(!res.ok) {
-        throw new Error('Something went wrong, please try again later.');
-      }
-      return res;
-    })
-    .then(res => res.json())
-    .then(data => {
-      this.setState({
-        search: "",
-        PrintType: [],
-        BookType: [],
-        error: null
-      });
-    })
-    .catch(err => {
-      this.setState({
-        error: err.message
-      });
-    });
+      .then(res => {
 
-    }
+        if (!res.ok) {
+          throw new Error('Something went wrong, please try again later.');
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(`GOT:`, data)
+        this.setState({
+          items: data.items,
+          // search: "",
+          // PrintType: [],
+          // BookType: [],
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+
+  }
 }
 
+export default GoogleApi
